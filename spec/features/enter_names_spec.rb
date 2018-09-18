2114d849d465a173ab7abae46788371fbbfc383f
Anjana <anjana.lpn@gmail.com>
2018-09-18 23:01:23 +0100

    User Story 2 As Player 1, So I can see how close I am to winning, I want to see Player 2's Hit Points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit '/'
-    fill_in :player_1_name, with: "Anj"
-    fill_in :player_2_name, with: "Bob"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Anj Vs Bob"
   end
 end
