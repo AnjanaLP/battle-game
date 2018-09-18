ce33721861017efaed98294defabb1b00f994244
Anjana <anjana.lpn@gmail.com>
2018-09-18 23:19:02 +0100

    User Story 3 As Player 1, So I can win a game of Battle, I want to attack Player 2, and I want to get a confirmation

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,7 @@
+feature 'Attacking' do
+  scenario 'attack player 2 and get confirmation' do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).to have_content "Anj attacked Bob"
+  end
+end
