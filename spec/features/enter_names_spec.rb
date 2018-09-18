bba88efac7580926c4b7c8b0b54d3963f13b1dcd
Anjana <anjana.lpn@gmail.com>
2018-09-18 22:52:21 +0100

    User Story 1 As two Players, So we can play a personalised game of Battle, We want to Start a fight by entering our Names and seeing them

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit '/'
+    fill_in :player_1_name, with: "Anj"
+    fill_in :player_2_name, with: "Bob"
+    click_button "Submit"
+    expect(page).to have_content "Anj Vs Bob"
+  end
+end
