e6055e6a7d666798197083da6b69b5d0b8d293b4
Anjana <anjana.lpn@gmail.com>
2018-09-19 19:45:36 +0100

    User Story 7 As Player 1, So I can lose a game of Battle, I want Player 2 to attack me, and I want to get a confirmation

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,7 +8,16 @@ feature 'Attacking' do
   scenario 'reduce player 2 hit points by 10' do
     sign_in_and_play
     click_button "Attack"
+    click_button "OK"
     expect(page).not_to have_content "Bob: 60HP"
     expect(page).to have_content "Bob: 50HP"
+  end
+
+  scenario 'attacked by player 2 and get confirmation' do
+    sign_in_and_play
+    click_button "Attack"
+    click_button "OK"
+    click_button "Attack"
+    expect(page).to have_content "Bob attacked Anj"
   end
 end
--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -7,6 +7,7 @@ feature 'Switch turns' do
   scenario 'it switches turn after player 1 attacks' do
     sign_in_and_play
     click_button "Attack"
+    click_button "OK"
     expect(page).not_to have_content "Anj's turn"
     expect(page).to have_content "Bob's turn"
   end
