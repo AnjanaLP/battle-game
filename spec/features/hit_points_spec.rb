f893faa3cc4b24d0cb02b225a8784ff579361e9f
Anjana <anjana.lpn@gmail.com>
2018-09-19 18:51:23 +0100

    User Story 6 As Player 1, So I can see how close I am to losing, I want to see my own hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'View hit points' do
     sign_in_and_play
     expect(page).to have_content "Bob: 60HP"
   end
+
+  scenario 'see player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content "Anj: 60HP"
+  end
 end
