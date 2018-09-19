d3a5bb1762c193b6b77d166e52fc096f8df4bddb
Anjana <anjana.lpn@gmail.com>
2018-09-19 15:45:52 +0100

    User Story 5 As two Players, So we can continue our game of Battle, We want to switch turns

---
+++ spec/features/switch_turns_spec.rb
@@ -0,0 +1,13 @@
+feature 'Switch turns' do
+  scenario 'it is player 1 turn at the start of the game' do
+    sign_in_and_play
+    expect(page).to have_content "Anj's turn"
+  end
+
+  scenario 'it switches turn after player 1 attacks' do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).not_to have_content "Anj's turn"
+    expect(page).to have_content "Bob's turn"
+  end
+end
